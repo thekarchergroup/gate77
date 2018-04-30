8e76f82935c220a1b6d28642ba2543e939c22f8b
Brock Duncan <bduncan@tkg.com>
2018-04-30 16:38:02 -0400

    add accordions/mixins partials, stat counters

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,25 @@
 // Styles
 import '../scss/master.scss';
 // Scripts
-import 'jquery';
-console.log('working');
+import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;
+
+$(function() {
+  // todo: check if in viewport
+  // 
+  // state counter
+  const counterSpeed = 70;
+  const counters = $('[data-counter]');
+  counters.each(function() {
+    let end = $(this).data('counter-end');
+    let counter = $(this);
+
+    // increment if in view
+    for(let i = 0; i <= end; i++) {
+      setTimeout(function() {
+        counter.html(i);
+      }, i * counterSpeed);
+    }
+  })
+
+});
