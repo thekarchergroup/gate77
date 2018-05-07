6242ff4cbe4bd7d12bb8d049d8b03beca8aad1e3
Brock Duncan <bduncan@tkg.com>
2018-05-07 12:40:18 -0400

    add required attribute on blur to name and email inputs

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,13 @@ import jQuery from "jquery";
 window.$ = window.jQuery = jQuery;
 // Scripts
 import './counters';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Form validation
+  const reqInputs = document.querySelectorAll('.req');
+  reqInputs.forEach((input) => {
+    input.addEventListener('blur', (e) => {
+      e.target.setAttribute('required', true);
+    })
+  })
+})
